f998a2e8e72908cdc27c857b7344bf70e892b045
protectinghome <ldwlee03@gmail.com>
2024-04-23 21:27:52 +0900

    2

---
+++ Middletest_expected2/Middletest_expected2.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std; 
+
+class Point{
+    int x, y;
+public:
+    Point(int a, int b) : x(a), y(b) {}
+    Point(const Point &obj) : x(obj.x), y(obj.y) {
+        cout << "P\n";
+    }
+};
+
+class Rec{
+    Point p1, p2;
+public:
+    Rec(Point pp1, Point pp2) : p1(pp1), p2(pp2) {}
+    Rec(const Rec &obj) :p1(obj.p1), p2(obj.p2) {
+        cout <<"R\n";
+    }
+};
+
+int main(void)
+{
+    Point pa(10, 20), pb(100, 200);
+    Point pc = pb;
+    Rec r1(pa, pb);
+    Rec r2(pb, pa);
+    Rec r3 = r1;
+
+    return 0;
+}
+
+/*
+P
+P
+P
+P
+P
+
+*/
